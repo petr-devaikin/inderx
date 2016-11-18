05959376b0fdb73374b4d1a96fc5057d33d2aa06
Petr Devaikin <p.devaikin@gmail.com>
2016-11-18 19:03:54 +0100

    ammy

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+var frontCard = document.getElementById("frontCard");
 
+var mc = new Hammer(frontCard);
+
+var THRESHOLD = 100;
+
+mc.on("pan", function(ev) {
+    $(frontCard)
+        .removeClass('released')
+        .css('left', ev.deltaX);
+
+    if (ev.deltaX < -THRESHOLD)
+        $(frontCard)
+            .addClass('disliked')
+            .removeClass('liked');
+    else if (ev.deltaX > THRESHOLD)
+        $(frontCard)
+            .removeClass('disliked')
+            .addClass('liked');
+
+    if (ev.isFinal) {
+        if (ev.deltaX > THRESHOLD)
+            likeCard();
+        else if (ev.deltaX < -THRESHOLD)
+            dislikeCard();
+        else {
+            $(frontCard)
+                .addClass('released')
+                .css('left', 0);
+        }
+    }
+});
+
+function likeCard() {
+    console.log('LIKE!');
+}
+
+function dislikeCard() {
+    console.log('DISLIKE!');
+}
