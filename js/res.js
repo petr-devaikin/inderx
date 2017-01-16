2bfe315e58c3a1f03a5241a931876b2e0ca3f1d5
Petr <petr@ArtificomesiMac.rome>
2017-01-16 17:36:17 +0100

    vis

--- js/res.js
+++ js/res.js
@@ -1,4 +1,4 @@
-var PIXEL_PER_SECOND = 10;
+var PIXEL_PER_SECOND = 40;
 
 data = data.filter(function(a) { return a.finish; });
 
@@ -33,4 +33,48 @@ shows
         if (Math.round(res % 60) < 10)
             s += "0";
         return s + Math.round(res % 60);
+    });
+
+var actions = shows.selectAll('.action').data(function(d) {
+        return d.actions.filter(function(a) {
+            return a.type == 'open details' ||
+                   a.type == 'close details' ||
+                   a.type == 'go to next photo' ||
+                   a.type == 'go to prev photo';
+        });
+    })
+    .enter()
+    .append('div')
+    .classed('action', true)
+    .classed('action--open', function(d) { return d.type == 'open details';})
+    .classed('action--close', function(d) { return d.type == 'close details';})
+    .classed('action--next', function(d) { return d.type == 'go to next photo';})
+    .classed('action--prev', function(d) { return d.type == 'go to prev photo';})
+    .style('left', function(d) {
+        var start = new Date(d3.select(this.parentNode).datum().start);
+        var t = new Date(d.time);
+        return Math.round(PIXEL_PER_SECOND * (t - start) / 1000) + 'px';
+    })
+    .html(function(d) {
+        var start = new Date(d3.select(this.parentNode).datum().start);
+        var t = new Date(d.time);
+        return '&nbsp;' + Math.round((t - start) / 100) / 10;
+    });
+
+var emotions = shows.selectAll('.emotion').data(function(d) { return d.emotions; })
+    .enter()
+    .append('div')
+    .classed('emotion', true)
+    .classed('emotion--up', function(d) { return d.type == 'emg_0';})
+    .classed('emotion--down', function(d) { return d.type == 'emg_1';})
+    .style('left', function(d) {
+        var start = new Date(d3.select(this.parentNode).datum().start);
+        var t = new Date(d.time);
+        return Math.round(PIXEL_PER_SECOND * (t - start) / 1000) + 'px';
+    })
+    .html(function(d) {
+        return '';
+        var start = new Date(d3.select(this.parentNode).datum().start);
+        var t = new Date(d.time);
+        return '&nbsp;' + Math.round((t - start) / 100) / 10;
     });
